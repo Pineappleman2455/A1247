a071c9072a0521cc57c10dc015272e96c499cf71
Dale Edward Anderson <ander732@LabImage2016-17>
2019-02-09 18:42:37 -0800

    finished

--- A1.c
+++ A1.c
@@ -8,12 +8,14 @@
 #include <sys/time.h>
 #include <inttypes.h>
 
-#define MAX_THREAD_COUNT 1
-#define MAX_TASK_COUNT 1
+#define MAX_THREAD_COUNT 9
+#define MAX_TASK_COUNT 3
 
 typedef struct{
 	int threadCount;
 	pthread_t threadId;
+	//pthread_mutex_t g_ThreadMutex;
+	//pthread_cond_t g_conditionVar;
 	int threadPolicy;
 	int threadPri;
 	long processTime;
@@ -34,8 +36,17 @@ void* threadFunction(void *arg);
 
 void InitGlobals(void){
 	for(int i = 0; i < MAX_THREAD_COUNT; i++){
-		g_ThreadMutex[i] = PTHREAD_MUTEX_INITIALIZER;
-		g_conditionVar[i] = PTHREAD_COND_INITIALIZER;
+		g_ThreadArgs[i].threadCount = i;
+		g_ThreadArgs[i].threadPri = 10;
+		if(i < 3){
+			g_ThreadArgs[i].threadPolicy = SCHED_FIFO;
+		}
+		else if(i >= 3 && i < 6){
+			g_ThreadArgs[i].threadPolicy = SCHED_RR;
+		}
+		else{
+			g_ThreadArgs[i].threadPolicy = SCHED_OTHER;
+		}
 	}
 }
 
@@ -63,72 +74,49 @@ void DisplayThreadArgs(ThreadArgs* myThreadArg){
 void DoProcess(void){
   unsigned int longVar = 1 ;
   //TODO: Increase the number of fs to take longer
-  while(longVar < 0xffff) longVar++;
+  while(longVar < 0xffffffff) longVar++;
 }
 
 int main (int argc, char *argv[]){
+
 	InitGlobals();
-	for(int i = 0; i < MAX_THREAD_COUNT; i++){
-		pthread_create(&(g_ThreadArgs[i].threadId), NULL, &threadFunction, &g_ThreadArgs[i]);
+		for(int i = 0; i < MAX_THREAD_COUNT; i++){
+		pthread_create(&(g_ThreadArgs[i].threadId), NULL, *threadFunction, &g_ThreadArgs[i]);
 	}
-	for(int i = 0; i < MAX_THREAD_COUNT; i++){
-		threadFunction(&g_ThreadArgs[i]);
-	}
-	for(int i = 0; i < MAX_THREAD_COUNT; i++){
-		pthread_cond_signal( &g_conditionVar[i]);
-	}
+	for(int i =0; i < MAX_THREAD_COUNT; i++){
+		pthread_cond_signal(&g_conditionVar[i]);
+}
 	for(int i = 0; i < MAX_THREAD_COUNT; i++){
 		pthread_join(g_ThreadArgs[i].threadId, NULL);
-	}
-	for(int i = 0; i< MAX_THREAD_COUNT; i++){
 		DisplayThreadArgs(&g_ThreadArgs[i]);
-	}
+}
 	exit(0);
-	/*3.	Assign 3 threads to SCHED_OTHER, another 3 to SCHED_FIFO and another 3 to SCHED_RR
-	4.	Signal the condition variable
-	5.	Call �pthread_join� to wait on the thread
-	6.	Display the stats on the threads*/
 }
 
 void* threadFunction(void *arg)
 {
 	ThreadArgs* myThreadArg;
-	myThreadArg = (ThreadArgs*)arg;
-	if(myThreadArg->threadCount < 3){
-		myThreadArg->threadPolicy = SCHED_FIFO;
-	}
-	else if(myThreadArg->threadCount >= 3 && myThreadArg->threadCount < 6){
-		myThreadArg->threadPolicy = SCHED_RR;
-	}
-	else{
-		myThreadArg->threadPolicy = SCHED_OTHER;
-	}
-  pthread_setschedparam(pthread_self(), myThreadArg->threadPolicy, &param);
-	pthread_mutex_lock( &g_ThreadMutex[myThreadArg->threadCount] );
-  pthread_cond_wait( &g_conditionVar[myThreadArg->threadCount], &g_ThreadMutex[myThreadArg->threadCount] );
-	struct timespec start, end;
-	clock_gettime(CLOCK_REALTIME, &start);
-	myThreadArg->startTime = start.tv_sec *1000000;
-	myThreadArg->startTime += start.tv_nsec/1000;
-	if(start.tv_nsec % 1000 >= 500 ) myThreadArg->startTime++;
+	myThreadArg = (ThreadArgs*) arg;
+	//struct sched_param param;
+	pthread_setschedparam(myThreadArg->threadId, myThreadArg->threadPolicy, myThreadArg->threadPri);
+	pthread_mutex_lock( &g_ThreadMutex[myThreadArg->threadCount]);
+  pthread_cond_wait( &g_conditionVar[myThreadArg->threadCount], &g_ThreadMutex[myThreadArg->threadCount]);
+	pthread_mutex_unlock( &g_ThreadMutex[myThreadArg->threadCount] );
+
+	struct timespec tms;
+	clock_gettime(CLOCK_REALTIME, &tms);
+	myThreadArg->timeStamp[0] = tms.tv_sec *1000000;
+	myThreadArg->timeStamp[0] += tms.tv_nsec/1000;
+	if(tms.tv_nsec % 1000 >= 500 ) myThreadArg->timeStamp[0]++;
 	int i = 0;
 	while(i < 3){
 		DoProcess();
 		i++;
-	}
-	clock_gettime(CLOCK_REALTIME, &end);
-	myThreadArg->endTime = end.tv_sec *1000000;
-	myThreadArg->endTime += end.tv_nsec/1000;
-	if(end.tv_nsec % 1000 >= 500 ) myThreadArg->endTime++;
-	pthread_mutex_unlock( &g_ThreadMutex[myThreadArg->threadCount] );
-	pthread_exit(NULL);
+		clock_gettime(CLOCK_REALTIME, &tms);
+		myThreadArg->timeStamp[i+1] = tms.tv_sec *1000000;
+		myThreadArg->timeStamp[i+1] += tms.tv_nsec/1000;
+		if(tms.tv_nsec % 1000 >= 500 ) myThreadArg->timeStamp[i+1]++;
 
-	/*1.	Typecast the argument to a �ThreadArgs*� variable
-	2.	Use the �pthread_setscheduleparam� API to set the thread policy
-	3.	Init the Condition variable and associated mutex
-	4.	Wait on condition variable
-	5.	Once condition variable is signaled, use the �time� function and the �clock_gettime(CLOCK_REALTIME, &tms)� to get timestamp
-	6.	Call �DoProcess� to run your task
-	7.	Use �time� and �clock_gettime� to find end time.
-	8.	You can repeat steps 6 and 7 a few times if you wish*/
+	}
+	//pthread_exit(NULL);
 }
